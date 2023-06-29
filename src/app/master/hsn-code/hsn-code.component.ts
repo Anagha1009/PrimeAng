9ee441962e36ff3517880cd136382f0e665c0d39
Anagha1009 <anaghat@portall.in>
2023-06-29 10:29:00 +0530

    HSN

--- src/app/master/hsn-code/hsn-code.component.ts
+++ src/app/master/hsn-code/hsn-code.component.ts
@@ -1,6 +1,6 @@
 import { formatDate } from '@angular/common';
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, FormArray,Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 import { CommonService } from 'src/app/services/common.service';
 import { MasterService } from 'src/app/services/master.service';
 import { MASTER } from 'src/app/models/master';
@@ -10,89 +10,52 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-hsn-code',
   templateUrl: './hsn-code.component.html',
-  styleUrls: ['./hsn-code.component.scss']
+  styleUrls: ['./hsn-code.component.scss'],
 })
 export class HsnCodeComponent implements OnInit {
-  hsnForm1 : FormGroup;
-  hsnForm : FormGroup;
+  hsnForm1: FormGroup;
+  hsnForm: FormGroup;
   submitted: boolean = false;
-  date:any
-  HsnList:any[]=[];
+  date: any;
+  HsnList: any[] = [];
   isUpdate: boolean = false;
   isLoading: boolean = false;
   isLoading1: boolean = false;
   master: MASTER = new MASTER();
 
-
-
   @ViewChild('closeBtn') closeBtn: ElementRef;
   @ViewChild('openModalPopup') openModalPopup: ElementRef;
-  constructor(private _formBuilder : FormBuilder, private _masterService : MasterService,
-    private _commonService : CommonService) { }
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _masterService: MasterService,
+    private _commonService: CommonService
+  ) {}
 
   ngOnInit(): void {
     this.hsnForm1 = this._formBuilder.group({
-      FROM_DATE:[''],
-      TO_DATE:['']
+      FROM_DATE: [''],
+      TO_DATE: [''],
     });
+
     this.hsnForm = this._formBuilder.group({
-      HSN_CODE:['',Validators.required],
-      HSN_DESC:[''],
-      CREATED_BY:[''],
-      CREATED_DATE:[''],
-      CODE_LIST: new FormArray([]),
+      HSN_CODE: ['', Validators.required],
+      HSN_DESC: [''],
+      RATE: ['', Validators.required],
+      EFFECTIVE_FROM: ['', Validators.required],
+      EFFECTIVE_TO: [''],
+      IGST: ['', Validators.required],
+      SGST: ['', Validators.required],
+      CGST: ['', Validators.required],
+      CREATED_BY: [''],
     });
 
-
-
-    const add = this.hsnForm.get('CODE_LIST') as FormArray;
-    add.push(
-      this._formBuilder.group({
-        RATE: ['',Validators.required],
-        EFFECTIVE_FROM: ['', Validators.required],
-        EFFECTIVE_TO: ['', Validators.required],
-        IGST:[0],
-        SGST:[0],
-        CGST:[0]
-
-
-      })
-    );
-
-  this.GetList();
+    this.GetList();
   }
   get f() {
     return this.hsnForm.controls;
   }
-  get f1() {
-    const add = this.hsnForm.get('CODE_LIST') as FormArray;
-    return add.controls;
-  }
 
-
-
-  addRow(){
-      this.submitted = true;
-    if (this.hsnForm.invalid) {
-      return;
-    }
-    const add = this.hsnForm.get('CODE_LIST') as FormArray;
-    add.push(
-      this._formBuilder.group({
-        RATE: ['',Validators.required],
-        EFFECTIVE_FROM: ['', Validators.required],
-        EFFECTIVE_TO: ['', Validators.required],
-        IGST:[0],
-        SGST:[0],
-        CGST:[0]
-
-
-      })
-    );
-  }
-
-
-  DeleteHSnMaster(ID: number){
+  DeleteHSnMaster(ID: number) {
     Swal.fire({
       title: 'Are you sure?',
       text: "You won't be able to revert this!",
@@ -107,91 +70,77 @@ export class HsnCodeComponent implements OnInit {
           if (res.ResponseCode == 200) {
             Swal.fire('Deleted!', 'Your record has been deleted.', 'success');
 
-              this.GetList();
+            this.GetList();
           }
         });
       }
     });
-
   }
 
-  openModal(){
+  openModal() {
     this.openModalPopup.nativeElement.click();
-}
-
-
-
+  }
 
   // search table data with date
-  Search(){
-    var FROM_DATE =  this.hsnForm1.value.FROM_DATE == null   ? ''  : this.hsnForm1.value.FROM_DATE;
-    var TO_DATE =  this.hsnForm1.value.TO_DATE == null  ? ''  : this.hsnForm1.value.TO_DATE;
+  Search() {
+    var FROM_DATE =
+      this.hsnForm1.value.FROM_DATE == null
+        ? ''
+        : this.hsnForm1.value.FROM_DATE;
+    var TO_DATE =
+      this.hsnForm1.value.TO_DATE == null ? '' : this.hsnForm1.value.TO_DATE;
 
- if ( FROM_DATE == '' && TO_DATE == '') {
-   alert('Please enter atleast one filter to search !');
- }
- this.master.FROM_DATE = FROM_DATE;
- this.master.TO_DATE = TO_DATE;
+    if (FROM_DATE == '' && TO_DATE == '') {
+      alert('Please enter atleast one filter to search !');
+    }
+    this.master.FROM_DATE = FROM_DATE;
+    this.master.TO_DATE = TO_DATE;
 
- this.isLoading = true;
+    this.isLoading = true;
   }
 
-  SubmitHSN(){
+  SubmitHSN() {
     this.submitted = true;
     if (this.hsnForm.invalid) {
       return;
-    }else{
-      this.hsnForm.get("CREATED_BY").setValue(this._commonService.getUserName());
-      this.hsnForm.get("CREATED_DATE").setValue(this._commonService.getcurrentDate(new Date()))
-      this._masterService.InsertHsnCode(JSON.stringify(this.hsnForm.value)).subscribe((res:any)=>{
-        console.log("form value of hsn code", this.hsnForm.value)
-        if (res.responseCode == 200) {
-          this._commonService.successMsg('Your record has been inserted successfully !');
-          this.closeBtn.nativeElement.click();
-          this.GetList()
-        }
+    } else {
+      this.hsnForm
+        .get('CREATED_BY')
+        .setValue(this._commonService.getUserName());
+
+      this._masterService
+        .InsertHsnCode(JSON.stringify(this.hsnForm.value))
+        .subscribe((res: any) => {
+          console.log('form value of hsn code', this.hsnForm.value);
+          if (res.responseCode == 200) {
+            this._commonService.successMsg(
+              'Your record has been inserted successfully !'
+            );
+            this.closeBtn.nativeElement.click();
+            this.GetList();
+          }
         });
     }
-
-  }
-
-  // delete modal pop row
-  Delete(i: number){
-       const add = this.hsnForm.get('CODE_LIST') as FormArray;
-    add.removeAt(i);
-
   }
 
   // bind list to table
-  GetList(){
+  GetList() {
     this._commonService.destroyDT();
-    this._masterService.getHsnList().subscribe((res:any)=>{
-      console.log("res =>", res.Data)
-      if(res.ResponseCode == 200){
-        this.HsnList = res.Data
-       }
-       this._commonService.getDT();
+    this._masterService.getHsnList().subscribe((res: any) => {
+      if (res.ResponseCode == 200) {
+        this.HsnList = res.Data;
+      }
+      this._commonService.getDT();
     });
   }
 
-  InsertHsnMaster(){
-  }
-  UpdateHSnMaster(){
-
-
-  }
-  ClearForm(){
+  ClearForm() {
     this.hsnForm.reset();
     this.hsnForm.get('ID')?.setValue(0);
-    this.hsnForm.get('HSN_CODE')?.setValue('');
-    this.hsnForm.get('HSN_DESC')?.setValue('');
-    this.hsnForm.get('CREATED_BY')?.setValue('');
-    this.hsnForm.get('CREATED_DATE')?.setValue('');
   }
 
-  Clear(){
+  Clear() {
     this.hsnForm1.reset();
     this.isLoading1 = true;
   }
-
 }
