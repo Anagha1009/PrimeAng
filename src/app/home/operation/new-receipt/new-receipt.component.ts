3779222fe1e768e373cf0a86563ae23c4fa44ac2
Anagha1009 <anaghat@portall.in>
2023-07-21 14:02:44 +0530

    receipt generation

--- src/app/home/operation/new-receipt/new-receipt.component.ts
+++ src/app/home/operation/new-receipt/new-receipt.component.ts
@@ -16,6 +16,9 @@ export class NewReceiptComponent implements OnInit {
   invoiceNoList: string = '';
   invoiceReceiptList: any;
   bankList: any[] = [];
+  invListDDL: any[] = [];
+  hideme: [];
+  subtableIndex: any = -1;
 
   constructor(
     private _formBuilder: FormBuilder,
@@ -27,16 +30,18 @@ export class NewReceiptComponent implements OnInit {
     this.receiptForm = this._formBuilder.group({
       BL_NO: [''],
       INVOICE_NO: [''],
+      INVOICE_PARTY: [''],
       RECEIVED_FROM: [''],
       INVOICE_LIST: new FormArray([]),
       BANK_LIST: new FormArray([]),
       CHARGE_LIST: new FormArray([]),
+      CHARGE_LIST1: new FormArray([]),
     });
 
     this.invoiceNo = localStorage.getItem('InvoiceNo');
-    var list: any = JSON.parse(localStorage.getItem('InvoiceNoList'));
+    this.invListDDL = JSON.parse(localStorage.getItem('InvoiceNoList'));
 
-    list.forEach((element: any) => {
+    this.invListDDL.forEach((element: any) => {
       this.invoiceNoList += element + ',';
     });
 
@@ -60,6 +65,18 @@ export class NewReceiptComponent implements OnInit {
             invList.push(this._formBuilder.group(element));
           });
 
+          this.receiptForm
+            .get('INVOICE_PARTY')
+            .setValue(
+              res.Data.INVOICE_LIST[0].SHIPPER == ''
+                ? res.Data.INVOICE_LIST[0].CONSIGNEE
+                : res.Data.INVOICE_LIST[0].SHIPPER
+            );
+
+          this.receiptForm
+            .get('RECEIVED_FROM')
+            .setValue(res.Data.INVOICE_LIST[0].BILL_FROM.split('|')[0]);
+
           const add = this.receiptForm.get('BANK_LIST') as FormArray;
           add.clear();
           add.push(
@@ -78,8 +95,12 @@ export class NewReceiptComponent implements OnInit {
           this.bankList = res.Data.BANK_LIST;
 
           const add1 = this.receiptForm.get('CHARGE_LIST') as FormArray;
+          const add2 = this.receiptForm.get('CHARGE_LIST1') as FormArray;
+          add1.clear();
+          add2.clear();
           res.Data.CHARGE_LIST.forEach((elem: any) => {
             add1.push(this._formBuilder.group(elem));
+            add2.push(this._formBuilder.group(elem));
           });
 
           // this.receiptForm
@@ -89,21 +110,17 @@ export class NewReceiptComponent implements OnInit {
       });
   }
 
-  addBank() {
-    const add = this.receiptForm.get('BANK_LIST') as FormArray;
+  getChargeList(invoiceNo: any, index: any) {
+    const add1 = this.receiptForm.get('CHARGE_LIST') as FormArray;
+    const add2 = this.receiptForm.get('CHARGE_LIST1') as FormArray;
 
-    add.push(
-      this._formBuilder.group({
-        INS_TYPE: [''],
-        BANK_NAME: [''],
-        INS_NO: [''],
-        INS_DATE: [''],
-        DEPOSIT_DATE: [''],
-        INS_AMOUNT: [''],
-        BANK_LOCATION: [''],
-        ISNEW: [1],
-      })
-    );
+    var x = add2.controls.filter((x) => x.value.INVOICE_NO == invoiceNo);
+
+    add1.clear();
+    x.forEach((control) => {
+      add1.push(control);
+    });
+    this.subtableIndex = this.subtableIndex == -1 ? index : -1;
   }
 
   get f() {
